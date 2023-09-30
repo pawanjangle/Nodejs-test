3cfa732a0e30bb6e9e01e8bb6c20c932a03cc6b1
pawan jangle <pawanjangle1000@gmail.com>
2023-09-30 09:00:12 +0530

    new project

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const express = require( "express" );
+const app = express();
+const bodyparser = require( "body-parser" );
+const cors = require( "cors" );
+
+app.use(express.json());
+app.use( bodyparser.urlencoded( {extended: false} ) );
+app.use(cors());
+
+app.get( "/", (req, res)=>{
+res.json("hello pawan")
+} );
+app.listen( 3000, (err) =>{
+if (err)
+{
+console.log( "error" )
+}
+else{
+console.log("express server is listening on port 3000")
+}
+} );
